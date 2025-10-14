e75c187cfa0ceef59a24d3f3bbc9bab4099d2445
Abu-muaz <plshireseid@gmail.com>
2025-10-14 19:56:56 +0300

    deployed

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -12,3 +12,24 @@ const dbConnection = mysql2.createPool({
 });
 
 module.exports = dbConnection.promise();
+
+
+
+
+// import mysql from "mysql2/promise";
+
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const db = await mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+// console.log("✅ Database connected successfully");
+
+
+// export default db;
